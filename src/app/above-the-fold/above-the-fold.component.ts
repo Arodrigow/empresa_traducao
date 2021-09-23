12c8b71a1564a93a44e878a2ab7304b67154a19f
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-23 20:10:52 -0300

    first commit

---
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-above-the-fold',
+  templateUrl: './above-the-fold.component.html',
+  styleUrls: ['./above-the-fold.component.css']
+})
+export class AboveTheFoldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboveTheFoldComponent
   ],
   imports: [
     BrowserModule
