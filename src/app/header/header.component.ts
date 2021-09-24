8ba4a82359f09c6a18a6e52ef0968f56e0fbc1c1
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-24 11:34:31 -0300

    [Update] Started mobile first design of header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  innerWidth: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = event.target.visualViewport.width;
+  }
+
+  isSmallScreen() {
+    if (innerWidth < 641) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
